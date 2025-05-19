627c810f51cf6afdbbd306678698aec0823645a4
Alpay Bilğay <alpaybil@hotmail.com>
2025-05-20 02:35:15 +0300

    slider bitti

--- frontend/src/components/Products/ProductItem.jsx
+++ frontend/src/components/Products/ProductItem.jsx
@@ -13,51 +13,38 @@ const ProductItem = ({ productItem }) => {
 
   const originalPrice = productItem.price.current;
   const discountPercentage = productItem.price.discount;
-  // İndirimli fiyatı hesaplama
   const discountedPrice =
     originalPrice - (originalPrice * discountPercentage) / 100;
+
   return (
-    <div className="product-item glide__slide glide__slide--active">
+    <div className="product-item">
       <div className="product-image">
-        <a href="#">
-          <img src={productItem.img[0]} alt="" className="img1" />
-          <img src={productItem.img[1]} alt="" className="img2" />
-        </a>
+        <img src={productItem.img[0]} alt={productItem.name} />
       </div>
+
       <div className="product-info">
-        <a href="$" className="product-title">
+        <a href="#" className="product-title">
           {productItem.name}
         </a>
         <ul className="product-star">
-          <li>
-            <i className="bi bi-star-fill"></i>
-          </li>
-          <li>
-            <i className="bi bi-star-fill"></i>
-          </li>
-          <li>
-            <i className="bi bi-star-fill"></i>
-          </li>
-          <li>
-            <i className="bi bi-star-fill"></i>
-          </li>
-          <li>
-            <i className="bi bi-star-half"></i>
-          </li>
+          {[...Array(4)].map((_, i) => (
+            <li key={i}><i className="bi bi-star-fill"></i></li>
+          ))}
+          <li><i className="bi bi-star-half"></i></li>
         </ul>
+
         <div className="product-prices">
           <strong className="new-price">${discountedPrice.toFixed(2)}</strong>
           <span className="old-price">${originalPrice.toFixed(2)}</span>
         </div>
-        <span className="product-discount">-{productItem.price.discount}%</span>
+
+        <span className="product-discount">-{discountPercentage}%</span>
+
         <div className="product-links">
           <button
             className="add-to-cart"
             onClick={() =>
-              addToCart({
-                ...productItem,
-                price: discountedPrice,
-              })
+              addToCart({ ...productItem, price: discountedPrice })
             }
             disabled={filteredCart}
           >
@@ -81,6 +68,5 @@ const ProductItem = ({ productItem }) => {
 export default ProductItem;
 
 ProductItem.propTypes = {
-  productItem: PropTypes.object,
-  setCartItems: PropTypes.func,
+  productItem: PropTypes.object.isRequired,
 };
