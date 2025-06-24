9a1a6e431c296c06c08234feedfffb3f0fe3ca81
Yunus Emre Koyun <yunusemrekoyun26@gmail.com>
2025-06-24 17:12:53 +0300

    .

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { message, Modal } from "antd";
-import "./login.css";
+import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
